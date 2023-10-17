85936aaeaec3e274ffa5e183630441b5886be630
Miledi75 <mileto.di.marco@howest.be>
2023-10-17 15:23:47 +0200

    Add project files.

---
+++ Pri.Generics.Cons/Program.cs
@@ -0,0 +1,13 @@
+﻿using Pri.Generics.Cons.Entities;
+using Pri.Generics.Cons.Generics;
+
+namespace Pri.Generics.Cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generics voorbeelden");
+        }
+    }
+}
